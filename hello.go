58619786569bd5b5f7edf336a7076bab569db866
Darkburning <1581690775@qq.com>
2023-03-11 22:44:15 +0800

    channel && 并发

--- hello.go
+++ hello.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math"
 	"time"
@@ -58,6 +59,7 @@ func intSeq() func() int { //返回值为：一个返回整型变量的函数
 }
 
 // 结构体、方法及接口
+// 在 Go 中，interface 类型表示了一组方法签名，任何实现了这些方法的类型都可以被赋值给该接口变量
 type geometry interface { // 定义一个几何体的接口
 	area() float32
 	peri() float32
@@ -67,6 +69,7 @@ type rect struct {
 	width  float32
 }
 
+// 相当于实现了rect对象的对应方法
 // 这里的 area 是一个拥有 *rect 类型接收器(receiver)的方法
 func (r rect) area() float32 { // 这里若带*号表示引用传递，无需再拷贝一次，且在函数内可改变对象的值，不带*号表示按值传递
 	return r.length * r.width
@@ -112,12 +115,47 @@ type container struct {
 	str string
 }
 
+// 错误处理
+// 错误通常是最后一个返回值并且是 error 类型，它是一个内建的接口
+func f1(arg int) (int, error) {
+	if arg == 42 {
+
+		return -1, errors.New("can't work with 42")
+
+	}
+
+	return arg + 3, nil
+}
+
+type argError struct {
+	arg  int
+	prob string
+}
+
+// 定义了结构体argError的Error方法
+func (e *argError) Error() string {
+	return fmt.Sprintf("%d - %s", e.arg, e.prob)
+}
+
+// 注意error是interface
+func f2(arg int) (int, error) {
+	if arg == 42 {
+
+		return -1, &argError{arg, "can't work with it"} //因为Error需要指针接收
+	}
+	return arg + 3, nil
+}
+
 // 用于go 并发
 func say(s string) {
-	for i := 0; i < 5; i++ {
+	for i := 0; i < 3; i++ {
 		time.Sleep(100 * time.Millisecond)
 		fmt.Println(s)
 	}
+}
+
+func send(msg chan string, str string) {
+	msg <- str
 }
 func main() { // go对大括号的放置十分严格，仅允许这样放置
 	fmt.Println("Hello, World!")
@@ -147,7 +185,7 @@ func main() { // go对大括号的放置十分严格，仅允许这样放置
 	//for true {
 	//	var i int
 	//	fmt.Println("Input a num:")
-	//	fmt.Scanf("%d", &i)
+	//fmt.Scanf("%d", &i)
 	//	switch i {
 	//	case 1:
 	//		fmt.Printf("%d is one\n", i)
@@ -205,27 +243,27 @@ func main() { // go对大括号的放置十分严格，仅允许这样放置
 	//fmt.Printf("sin(1) is: %f \n", math.Sin(1))
 
 	// 结构体、方法及接口
-	rect1 := rect{2, 3}
-	circle1 := circle{3}
-	fmt.Printf("Area is %f, Peri is %f\n", rect1.area(), rect1.peri())
-	measure(rect1)
-	measure(circle1)
-
-	// 嵌入（Embedding）
-	co := container{base: base{num: 1},
-		str: "some name",
-	}
-	// 可直接访问嵌入其内的数据 等效于 fmt.Println("also num:", co.base.num)
-	fmt.Printf("co={num: %v, str: %v}\n", co.num, co.str) //当创建含有嵌入的结构体，必须对嵌入进行显式的初始化
-	// 也可以调用嵌入其中的数据
-	fmt.Println("describe:", co.describe())
-	// 定义一个接口
-	type describer interface {
-		describe() string
-	}
-	// co的嵌入已经实现接口故可直接调用接口方法
-	var d describer = co
-	fmt.Println("describer:", d.describe())
+	//rect1 := rect{2, 3}
+	//circle1 := circle{3}
+	//fmt.Printf("Area is %f, Peri is %f\n", rect1.area(), rect1.peri())
+	//measure(rect1)
+	//measure(circle1)
+	//
+	//// 嵌入（Embedding）
+	//co := container{base: base{num: 1},
+	//	str: "some name",
+	//}
+	//// 可直接访问嵌入其内的数据 等效于 fmt.Println("also num:", co.base.num)
+	//fmt.Printf("co={num: %v, str: %v}\n", co.num, co.str) //当创建含有嵌入的结构体，必须对嵌入进行显式的初始化
+	//// 也可以调用嵌入其中的数据
+	//fmt.Println("describe:", co.describe())
+	//// 定义一个接口
+	//type describer interface {
+	//	describe() string
+	//}
+	//// co的嵌入已经实现接口故可直接调用接口方法
+	//var d describer = co
+	//fmt.Println("describer:", d.describe())
 
 	//// map 构造方法
 	//myMap := make(map[string]int)
@@ -277,8 +315,42 @@ func main() { // go对大括号的放置十分严格，仅允许这样放置
 	//t := intSeq()
 	//fmt.Println(t())
 
-	//go 并发
-	//go say("Hello")
-	//go say("World")
-	//say("!")
+	//for _, i := range []int{7, 42} {
+	//	if r, e := f1(i); e != nil { //此处相当于先执行 r, e := f1(i)然后进行判断,这样写更紧凑
+	//		fmt.Println("f1 failed:", e)
+	//	} else {
+	//		fmt.Println("f1 worked:", r)
+	//	}
+	//}
+	//for _, i := range []int{7, 42} {
+	//	if r, e := f2(i); e != nil {
+	//		fmt.Println("f2 failed:", e)
+	//	} else {
+	//		fmt.Println("f2 worked:", r)
+	//	}
+	//}
+	//// 由于argError实现了error这个interface的方法因此可以将argError赋值给interface，此时e是一个*argError类型变量
+	//_, e := f2(42)
+	//if ae, ok := e.(*argError); ok { //此处进行了类型断言，e.(*argError)在判断e是否是*argError类型
+	//	fmt.Println(ae.arg) //如果是则将argError赋值给ae并将ok置为true，否则ae将被置为nil，ok置为false
+	//	fmt.Println(ae.prob)
+	//}
+
+	//go channel
+	//默认发送和接收操作是阻塞的，直到发送方和接收方都就绪。这个特性允许我们，不使用任何其它的同步操作,就可以在程序结尾处等待消息
+	//var inputMsg string
+	//fmt.Scanf("%s", &inputMsg)
+	//message := make(chan string) // 使用make(chan val-type)创建一个接受string的channel
+	//go send(message, inputMsg)   //将"ping"发送到message这个channel
+	//msg := <-message             // 定义string类型的msg接受message的值
+	//fmt.Println(msg)
+
+	//通道缓冲
+	//默认情况下，通道是无缓冲的，这意味着只有对应的接收（<- chan）
+	//通道准备好接收时，才允许进行发送（chan <-）。有缓冲通道 允许在没有对应接收者的情况下，缓存一定数量的值
+	message := make(chan string, 2)
+	message <- "buffered"
+	message <- "channel"
+	fmt.Println(<-message) //由于通道是有缓冲的， 因此我们可以将这些值发送到通道中，而无需并发的接收。
+	fmt.Println(<-message)
 }
