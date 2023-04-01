704ce8778e65cc7820c85e614a9315f48b6fccf7
Darkburning <1581690775@qq.com>
2023-04-01 11:01:06 +0800

    修正Gin

--- LearnGin/WeatherAPI.go
+++ LearnGin/WeatherAPI.go
@@ -1,6 +1,6 @@
 package main
 
-// 不知道为什么会返回空数据...
+// 注意API格式
 import (
 	"compress/gzip"
 	"encoding/json"
@@ -10,6 +10,8 @@ import (
 	"net/http"
 )
 
+const ApiKey = "1ac7e50c9e8245f5aa8846800e117502"
+
 type Daily struct {
 	FxDate   string `json:"fxDate"`
 	TempMax  string `json:"tempMax"`
@@ -17,16 +19,16 @@ type Daily struct {
 	Humidity string `json:"humidity"`
 }
 type Weather struct {
-	Today [1]Daily `json:"daily"`
+	Today []Daily `json:"daily"`
 }
 
 func main() {
 	router := gin.Default()
 
-	router.GET("/weather/:city", func(c *gin.Context) {
+	router.GET("/weather/:city", func(c *gin.Context) { // 城市搜索也可以通过请求一个API来找到
 		city := c.Param("city")
-		apiKey := "1ac7e50c9e8245f5aa8846800e117502"
-		apiUrl := fmt.Sprintf("https://devapi.qweather.com/v7/weather/3d?location=%s&key=%s", city, apiKey)
+		city = city[1:] // bug来源: 此处返回的值会包含冒号而原来请求的API是不包含冒号的
+		apiUrl := fmt.Sprintf("https://devapi.qweather.com/v7/weather/3d?location=%v&key=%v", city, ApiKey)
 
 		response, err := http.Get(apiUrl)
 
@@ -35,41 +37,35 @@ func main() {
 			return
 		}
 		defer response.Body.Close()
-		fmt.Println("getDone")
 
-		var reader io.ReadCloser
-
+		// 和风天气使用了gzip压缩
 		// 检查响应头中是否包含Content-Encoding为gzip的信息，
 		// 如果包含，则使用gzip.NewReader函数创建一个gzip.Reader对象来解析响应体数据
+		var reader io.ReadCloser = response.Body
 		if response.Header.Get("Content-Encoding") == "gzip" {
 			reader, err = gzip.NewReader(response.Body)
 			if err != nil {
 				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 				return
 			}
-		} else {
-			reader = response.Body
+			defer reader.Close()
 		}
 
-		defer reader.Close()
-		fmt.Println("gzipDone")
-
-		bodyBytes, err := io.ReadAll(reader)
+		bytesData, err := io.ReadAll(reader)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
 		}
-		fmt.Println("readAllDone")
 
 		var weatherData Weather
 
-		err = json.Unmarshal(bodyBytes, &weatherData)
+		// 反序列化
+		err = json.Unmarshal(bytesData, &weatherData)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
 		}
-		fmt.Println(weatherData.Today[0])
-		c.JSON(http.StatusOK, gin.H{"city": city, "tempMax": weatherData.Today[0].TempMax, "tempMin": weatherData.Today[0].TempMin, "humidity": weatherData.Today[0].Humidity})
+		c.JSON(http.StatusOK, gin.H{"city": 101010100, "tempMax": weatherData.Today[0].TempMax, "tempMin": weatherData.Today[0].TempMin, "humidity": weatherData.Today[0].Humidity})
 	})
 
 	router.Run(":8080")
--- LearnGin/demo.go
+++ LearnGin/demo.go
@@ -11,38 +11,71 @@ func main() {
 	r := gin.Default()
 	// 添加一个icon
 	r.Use(favicon.New("./favicon.ico"))
-	// 访问地址，处理请求，包括POST，PUT和DELETE
 
-	// 返回json
-	//r.GET("/ping", func(c *gin.Context) {
-	//	c.JSON(http.StatusOK, gin.H{
-	//		"message": "pong",
-	//	})
-	//})
-
+	// 加载静态资源文件如：.css, .js 以及图片
+	r.Static("/static", "./static") // HTML中所有以/static开头的文件都去demo.go同级目录下的static去找
 	// 加载静态页面
 	r.LoadHTMLGlob("templates/*")
-	// 加载资源文件
-	r.Static("/static", "./static")
-	// 响应页面
+
+	// 响应HTML页面
 	r.GET("/index", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{
 			"msg": "This is the data from backend!",
 		})
 	})
 
+	// 访问地址，处理请求，包括GET,POST，PUT和DELETE
+	r.GET("/get", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{
+			"message": "GET",
+		})
+	})
+
+	r.POST("/post", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{ //处理JSON可用map（也即gin.H // ）或者结构体
+			"message": "POST",
+		})
+	})
+
+	r.DELETE("/delete", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{
+			"message": "DELETE",
+		})
+	})
+
+	r.PUT("/put", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{
+			"message": "PUT",
+		})
+	})
+
 	// 获取请求参数
 	// 法一：url?userid=xxx&username=xxx
 	r.GET("/user/info", func(context *gin.Context) {
-		userid := context.Query("userid")
+		userid := context.DefaultQuery("userid", "000000") // 默认Query,如果没有请求userid则会自动返回其值会默认为000000
 		username := context.Query("username")
 		context.JSON(http.StatusOK, gin.H{
 			"userid from frontend":   userid,
 			"username from frontend": username,
 		})
 	})
+	// 表单验证
+	r.GET("/login", func(context *gin.Context) {
+		context.HTML(http.StatusOK, "getForm.html", nil)
+	})
+	// 一次请求一个响应，验证登录时应该再发起一个服务
+	r.POST("/login", func(context *gin.Context) {
+		password := context.PostForm("password")
+		username := context.PostForm("username")
 
-	// 法二：user/info/userid/username
+		if password == "123456" && username == "kingkia" {
+			context.HTML(http.StatusOK, "index.html", gin.H{ // 返回的是HTML,
+				"msg": "Success!",
+			})
+		}
+	})
+
+	// 法二：user/info/userid/username 推荐使用
 	r.GET("/user/info/:userid/:username", func(context *gin.Context) {
 		userid := context.Param("userid")
 		username := context.Param("username")
